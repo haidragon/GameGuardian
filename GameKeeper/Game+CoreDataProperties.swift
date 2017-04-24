8b954aa4877e480a5ea43aa7afaec77a7f43e009
phil <phil@phils-mbp.home>
2017-04-24 16:33:17 -0400

    Replaced Core Data Files

---
+++ GameKeeper/Game+CoreDataProperties.swift
@@ -0,0 +1,40 @@
+//
+//  Game+CoreDataProperties.swift
+//  GameKeeper
+//
+//  Created by phil on 4/19/17.
+//  Copyright © 2017 Brook Street Games. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Game {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Game> {
+        return NSFetchRequest<Game>(entityName: "Game");
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var dateStarted: NSDate?
+    @NSManaged public var players: NSSet?
+
+}
+
+// MARK: Generated accessors for players
+extension Game {
+
+    @objc(addPlayersObject:)
+    @NSManaged public func addToPlayers(_ value: Player)
+
+    @objc(removePlayersObject:)
+    @NSManaged public func removeFromPlayers(_ value: Player)
+
+    @objc(addPlayers:)
+    @NSManaged public func addToPlayers(_ values: NSSet)
+
+    @objc(removePlayers:)
+    @NSManaged public func removeFromPlayers(_ values: NSSet)
+
+}
